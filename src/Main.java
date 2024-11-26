b83b469ab31d1464e2ee8662e5fbe97bbfc2a405
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2024-11-26 11:57:15 +0300

    JAVA EXAMPLE

---
+++ src/Main.java
@@ -0,0 +1,138 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+//        system.in sistem benden deer beklcek demek
+//        eğer iki ismim varsa ikinci ismi de alırsın
+
+
+//        System.out.print("isminizi giriniz:");
+//        String a = scn.next();
+//        System.out.print("yaşınızı giriniz:");
+//        int b = scn.nextInt();
+//        System.out.print(" merhaba benim adım " + a + " yasım " + b + " hoş geldiniz");
+
+
+//        System.out.print("doğum yılınızı giriniz:");
+//        int n = scn.nextInt();
+//        int b;
+//        b=2024-n;
+//        System.out.print("yaşınız " + b);
+
+//        System.out.print("celcius değerini giriniz:");
+//        double celcius = scn.nextDouble();
+//        double f= (celcius * 9/5)+32;
+//        System.out.println("fahrenheit değeri " + f);
+//
+
+
+
+
+//if elseif else  lü yapısının kullanılmasının sebebi if yaparsak hepsini olsa da olmasa da hepsini tek tek kontrol edecek
+
+//        System.out.println("a sayısını giriniz:");
+//        int a = scn.nextInt();
+//        System.out.println("b sayıyı giriniz:");
+//        int b = scn.nextInt();
+//        if (a < b) {
+//            System.out.println("a b'den küçüktür");
+//        } else if (b < a) {
+//            System.out.println("a b'den büyüktür ");
+//        } else {
+//            System.out.println("iki sayı birbirine eşittir");
+//        }
+
+//        System.out.println("1. sayıyı giriniz:");
+//        int n = scn.nextInt();
+//        System.out.println("2. sayıyı giriniz:");
+//        int m = scn.nextInt();
+//        System.out.println("3. sayıyı giriniz:");
+//        int s = scn.nextInt();
+//        int maxS=n;
+//        if (maxS<m) {
+//            maxS=m;
+//        }
+//        if(maxS<s) {
+//            maxS=s;
+//        }
+//
+//
+//        System.out.println("en büyük: " + maxS );
+
+
+
+
+//        System.out.println("üçgenin taban uzunluğunu giriniz:");
+//        int taban =scn.nextInt();
+//        System.out.println("üçgenin yüksekliğini giriniz:");
+//        int yuksekli =scn.nextInt();
+//        int alan= taban*yuksekli/2;
+//        System.out.println(alan);
+
+
+//        System.out.println("birinci sayııyı giriniz: ");
+//        int sayi1 = scn.nextInt();
+//        System.out.println("ikinci sayıyı giriniz: ");
+//        int sayi2 = scn.nextInt();
+//
+//        System.out.println("işlem giriniz: \n " +"1:toplama \n "+ "2:çıkarma \n" + "3:çarpma \n" + "4: bölme");
+//        int değer=scn.nextInt();
+//        if (değer==1){
+//            System.out.println(sayi1+sayi2);
+//        }
+//        if(değer ==2){
+//            System.out.println(sayi1-sayi2);
+//        }
+//        if(değer==3){
+//            System.out.println(sayi1*sayi2);
+//        }
+//        if (değer == 4) {
+//            System.out.println(sayi1/sayi2);
+//        }
+
+
+//        System.out.println("vize notunuzu giriniz:");
+//        int v = scn.nextInt();
+//        System.out.println("final noyunuzu giriniz:");
+//        int f = scn.nextInt();
+//        double sonuC= (v*0.4) + (f*0.6);
+//        if (sonuC >= 90){
+//            System.out.println("AA");
+//        }
+//        else if (sonuC>= 85 && sonuC<=89) {
+//            System.out.println("BA");
+//        }
+//        else if (sonuC>=80 && sonuC<=84) {
+//            System.out.println("BB");
+//        }
+//        else if (sonuC>=75 && sonuC<=79) {
+//            System.out.println("CB");
+//        }
+//        else if (sonuC>=70 && sonuC<=74) {
+//            System.out.println("CC");
+//        }
+//        else if (sonuC>=65 && sonuC<=69) {
+//            System.out.println("DC");
+//        }
+//        else if (sonuC>=60 && sonuC<=64) {
+//            System.out.println("DD");
+//        }
+//        else {
+//            System.out.println("FF");
+//        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
